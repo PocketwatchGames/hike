5a2ac98838256b802503aed4434dcbc255d48829
ajschatz <andy@pocketwatchgames.com>
2026-04-07 16:30:22 -0700

    moved weapon code to its own file

--- scripts/gameplay/Player.cs
+++ scripts/gameplay/Player.cs
@@ -21,15 +21,10 @@ public partial class Player : CharacterBody3D
 	readonly List<TallGrass> _tallGrassCollisions = new();
 	float _terrainSpeed = 1f;
 	bool _grounded;
-	readonly WeaponState[] _weapons = new WeaponState[(int)EItemSlot.Count];
-	int? _activeWeaponSlot;
-	ulong _weaponPressTime;
-	ulong _weaponActivateTime;
 
 	Vector3 _inputMove = Vector3.Zero;
 	Vector3 _inputLook = Vector3.Zero;
 	bool _lastInputWasGamepad;
-	static readonly string[] _weaponActions = new[] { "AttackMelee", "AttackRanged" };
 
 
 	public override void _Ready()
@@ -217,16 +212,6 @@ public partial class Player : CharacterBody3D
 		}
 		_lastInputWasGamepad = move != Vector2.Zero || look != Vector2.Zero;
 	}
-	
-	int? GetActiveWeapon()
-	{
-		if (_activeWeaponSlot.HasValue)
-		{
-			WeaponState activeWeapon = _weapons[_activeWeaponSlot.Value];
-			return activeWeapon != null && activeWeapon.data != null && Time.GetTicksMsec() < _weaponActivateTime + activeWeapon.data.activeTime ? _activeWeaponSlot : null;
-		}
-		return null;
-	}
 
 	private void UpdateHighlightInteractive()
 	{
@@ -279,180 +264,6 @@ public partial class Player : CharacterBody3D
 		if (area is InteractiveBox box && box.Interactive != null)
 		{
 			_interactiveCollisions.Remove(box.Interactive);
-		}
-	}
-
-	void WeaponActivate(WeaponState weapon, int slot)
-	{
-		ulong curTime = Time.GetTicksMsec();
-		weapon.lastWeaponEventIndex = -1;
-		_weaponActivateTime = curTime;
-		_activeWeaponSlot = slot;
-		weapon.cooldownTime = curTime + (ulong)(1000 * (weapon.data.cooldownTime + weapon.data.activeTime));
-		ProcessWeaponEvents(weapon, slot);
-	}
-	void WeaponPress(WeaponState weapon, int slot)
-	{
-		ulong curTime = Time.GetTicksMsec();
-		int? activeWeapon = GetActiveWeapon();
-		if (weapon.cooldownTime > curTime || weapon.data == null || activeWeapon.HasValue)
-		{
-			return;
-		}
-
-		if (weapon.data.activateOnRelease)
-		{
-			_weaponActivateTime = curTime;
-			_weaponPressTime = curTime;
-			_activeWeaponSlot = slot;
-		}
-		else
-		{
-			WeaponActivate(weapon, slot);
-		}
-	}
-
-	void WeaponRelease(WeaponState weapon, int slot)
-	{
-		if (weapon.data == null || _activeWeaponSlot != slot)
-		{
-			return;
-		}
-		if (weapon.data.activateOnRelease)
-		{
-			WeaponActivate(weapon, slot);
-		}
-	}
-
-	void DoWeaponEvent(WeaponState weapon, EItemSlot slot, WeaponEvent weaponEvent)
-	{
-		switch (weaponEvent.type)
-		{
-			case EWeaponEventType.Melee:
-				DoMeleeWeaponEvent(weapon, weaponEvent);
-				break;
-			case EWeaponEventType.Hitscan:
-				DoHitscanWeaponEvent(weapon, weaponEvent);
-				break;
-		}
-	}
-
-	void DoMeleeWeaponEvent(WeaponState weapon, WeaponEvent weaponEvent)
-	{
-		Vector3 damagePos = GlobalPosition + Vector3.Up + GlobalTransform.Basis.Z * weaponEvent.meleeRange;
-		var query = new PhysicsShapeQueryParameters3D();
-		query.Shape = new SphereShape3D() { Radius = weaponEvent.meleeRadius };
-		query.Transform = new Transform3D(Basis.Identity, damagePos);
-		query.CollisionMask = (uint)ECollisionLayer.HurtBox;
-		query.CollideWithAreas = true;
-		query.CollideWithBodies = false;
-
-		var results = GetWorld3D().DirectSpaceState.IntersectShape(query);
-		foreach (var result in results)
-		{
-			var collider = result["collider"].Obj;
-			if (collider is HurtBox hurtBox && hurtBox != _hurtBox)
-			{
-				hurtBox.Hit(weapon.data.damageData, this);
-			}
-		}
-
-		DebugSphere.Create(
-			_world,
-			new Color(1f, 0f, 0f, 0.3f),
-			0.15f,
-			damagePos,
-			weaponEvent.meleeRadius
-		);
-	}
-
-	void DoHitscanWeaponEvent(WeaponState weapon, WeaponEvent weaponEvent)
-	{
-		Vector3 origin = GlobalPosition + Vector3.Up;
-		Vector3 direction = GlobalTransform.Basis.Z;
-		Vector3 rayEnd = origin + direction * weaponEvent.hitScanRange;
-
-		var spaceState = GetWorld3D().DirectSpaceState;
-
-		Godot.Collections.Array<Rid> bodyExclude = [GetRid()];
-
-		// Find the nearest environment hit to clip the ray against world geometry.
-		var envQuery = PhysicsRayQueryParameters3D.Create(origin, rayEnd);
-		envQuery.CollisionMask = (uint)ECollisionLayer.Environment;
-		envQuery.CollideWithAreas = false;
-		envQuery.CollideWithBodies = true;
-		envQuery.Exclude = bodyExclude;
-		var envResult = spaceState.IntersectRay(envQuery);
-
-		Vector3 hitPos = rayEnd;
-		if (envResult.Count > 0)
-		{
-			hitPos = (Vector3)envResult["position"];
-		}
-
-		// Cast against hurtboxes up to the clipped end point.
-		var hurtQuery = PhysicsRayQueryParameters3D.Create(origin, hitPos);
-		hurtQuery.CollisionMask = (uint)ECollisionLayer.HurtBox;
-		hurtQuery.CollideWithAreas = true;
-		hurtQuery.CollideWithBodies = false;
-		if (_hurtBox != null)
-		{
-			hurtQuery.Exclude = [_hurtBox.GetRid()];
-		}
-
-		var hurtResult = spaceState.IntersectRay(hurtQuery);
-		if (hurtResult.Count > 0)
-		{
-			var collider = hurtResult["collider"].Obj;
-			if (collider is HurtBox hurtBox && hurtBox != _hurtBox)
-			{
-				hurtBox.Hit(weapon.data.damageData, this);
-				hitPos = (Vector3)hurtResult["position"];
-			}
-		}
-
-		DebugBox.Create(
-			_world,
-			new Color(1f, 0f, 0f, 0.3f),
-			0.15f,
-			origin,
-			hitPos,
-			0.1f,
-			0.1f
-		);
-	}
-
-	void ProcessWeaponEvents(WeaponState weapon, int slot)
-	{
-		if (weapon.data == null || weapon.data.events == null)
-		{
-			return;
-		}
-		ulong curTime = Time.GetTicksMsec();
-		for (int i = weapon.lastWeaponEventIndex + 1; i < weapon.data.events.Count; i++)
-		{
-			WeaponEvent weaponEvent = weapon.data.events[i];
-			if (curTime >= _weaponActivateTime + weaponEvent.time)
-			{
-				DoWeaponEvent(weapon, (EItemSlot)slot, weaponEvent);
-				weapon.lastWeaponEventIndex = i;
-			}
-			else
-			{
-				break;
-			}
-		}
-	}
-
-	void ProcessWeapon(WeaponState weapon, int slot, bool inputPressed, float dt)
-	{
-		if (weapon == null || weapon.data == null)
-		{
-			return;
-		}
-		if (_activeWeaponSlot == slot)
-		{
-			ProcessWeaponEvents(weapon, slot);
 		}
 	}
 
